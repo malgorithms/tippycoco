fc61bfb4a79e64bdf4d7416cbaa062180712e1d0
Chris Coyne <ccoyne77@gmail.com>
2023-02-03 09:13:16 -0500

    wip

--- src/page-ui.ts
+++ src/page-ui.ts
@@ -126,7 +126,7 @@ class PageUi {
       <p>
         This game is a free, open-source hobby project by <b>Chris Coyne</b> (<a href="https://chriscoyne.com">chriscoyne.com</a>) with help from friends. The GitHub readme shows how to run your own
         copy of the game. It's a joy to change the game physics and rules. The original inspiration for this game was Slime Volleyball, by Quin Pendragon. Music in T.C.F.T.G. by my friend
-        Christian Rudder, of the band <a href="https://en.wikipedia.org/wiki/Bishop_Allen">Bishop Allen</a>.
+        Christian Rudder, of the band <a href="https://en.wikipedia.org/wiki/Bishop_Allen">Bishop Allen</a>. "Rejected!" and "Slam!" voiced by Jennie and Cameron Coyne.
       </p>
       </div>
     </div>`
